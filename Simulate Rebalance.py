b6afef9ceaef745cf117638ff919a8723c364db8
Carter Carlson <carterbud1@gmail.com>
2018-08-23 16:30:21 -0500

    Optimize simulation to utilize numpy functions/remove update_weights function

--- Simulate Rebalance.py
+++ Simulate Rebalance.py
@@ -5,27 +5,20 @@ import ccxt
 import os
 import time
 import sys
-
-def update_weight_and_price(num_day):
-    data[3] = list(np.multiply(small_historical_prices[num_day], data[1]))
-    total = sum(data[3])
-    data[2] = list(np.divide(data[3], total))
-    return total
-
+import timeit
 
 def update_quantity(add_side, add_amt, subtract_side, subtract_amt):
-    data[1][data[0].index(add_side)] += add_amt
-    data[1][data[0].index(subtract_side)] -= subtract_amt
+    coin_amts[coin_list.index(add_side)] += add_amt
+    coin_amts[coin_list.index(subtract_side)] -= subtract_amt
 
-
-t0 = time.time()
 historical_prices = pd.read_csv('historical_data.csv')
 exchange = ccxt.binance()
 tickers = set()
-[tickers.add(ticker) for ticker in exchange.fetchTickers()]
+[tickers.add(ticker) for ticker in exchange.fetch_tickers()]
 
 start_amt = 5000
 thresh = .05
+t0 = time.time()
 
 for num_coins in range(2,11,2):
     avg_weight = 1/num_coins
@@ -47,22 +40,26 @@ for num_coins in range(2,11,2):
         fees = 0
         trade_count = 0
         trades_eliminated = 0
-        data = []
+
         daily_totals = [start_amt]
         col_name = cols[num_simulation]
         coin_list = col_name.split('-')
         coin_amts = [amt_each / historical_prices[i][0] for i in coin_list]
+        weights = np.multiply(avg_weight, num_coins)
 
         small_historical_prices = np.array(historical_prices[coin_list])
-        data = [coin_list, coin_amts, [avg_weight] * num_coins]
-        data.append([amt_each] * num_coins)
+
         # num_day = 1
         for num_day in range(1,len(historical_prices)):
             while True:
-                total_dollar_value = update_weight_and_price(num_day)
 
-                light_weight, heavy_weight = min(data[2]), max(data[2])
-                light_coin, heavy_coin = data[0][data[2].index(light_weight)], data[0][data[2].index(heavy_weight)]
+                dollar_values = np.multiply(small_historical_prices[num_day], coin_amts).tolist()
+                total_dollar_value = sum(dollar_values)
+                weights = np.divide(dollar_values, total_dollar_value).tolist()
+
+
+                light_weight, heavy_weight = min(weights), max(weights)
+                light_coin, heavy_coin = coin_list[weights.index(light_weight)], coin_list[weights.index(heavy_weight)]
 
                 if heavy_weight - light_weight < 2 * avg_weight * thresh:
                     break
@@ -72,10 +69,10 @@ for num_coins in range(2,11,2):
                     weight_to_sell = (avg_weight - light_weight)
 
                 ratios = [light_coin + '/' + heavy_coin, heavy_coin + '/' + light_coin]
-                ticker = list((set(ratios) & tickers))
+                ticker = list(set(ratios) & tickers)
                 dollar_amt = weight_to_sell * total_dollar_value
 
-                if len(ticker) < 1:
+                if not ticker:
                     ticker = [heavy_coin + '/BTC', light_coin + '/BTC']
                     denom = ticker[1][:ticker[1].find('/')]
                     trade_count += 2
@@ -88,8 +85,8 @@ for num_coins in range(2,11,2):
                 rate = len(ticker) * .0025
                 fees += (dollar_amt * rate)
 
-                numer_quantity = np.divide(dollar_amt, small_historical_prices[num_day][data[0].index(numer)])
-                denom_quantity = np.divide(dollar_amt, small_historical_prices[num_day][data[0].index(denom)])
+                numer_quantity = np.divide(dollar_amt, small_historical_prices[num_day][coin_list.index(numer)])
+                denom_quantity = np.divide(dollar_amt, small_historical_prices[num_day][coin_list.index(denom)])
                 denom_quantity_after_fees = (1-rate) * denom_quantity
 
                 if numer == light_coin:
@@ -98,7 +95,7 @@ for num_coins in range(2,11,2):
                     update_quantity(denom, denom_quantity_after_fees, numer, numer_quantity)
 
             # document total portfolio value on that day
-            daily_totals.append(sum(small_historical_prices[num_day] * data[1]))
+            daily_totals.append(sum(small_historical_prices[num_day] * coin_amts))
 
         # Add year of rebalancing simulation to simulation dataset
         rebalance_simulations[col_name] = daily_totals
@@ -114,8 +111,6 @@ for num_coins in range(2,11,2):
     rebalance_simulations.to_csv(path + 'rebalanced.csv')
     simulation_summary.to_csv(path + 'summary.csv')
 
-print('Time to run (minutes): {:2.2f}'.format((time.time()-t0)/60))
-
     # average simulation time - 8/16
     # 2   -  60/min
     # 4   -  50/min
@@ -127,3 +122,7 @@ print('Time to run (minutes): {:2.2f}'.format((time.time()-t0)/60))
     # 8/21 - refactored pandas DataFrames into numpy arrays
     # 2 - 4,200/min
     # 4 - 3,000/min
+
+    # 8/23 - refactored list of lists (data) to separate numpy lists / removed update_weight function
+    # 2 - 8,000/min
+    # 4 - 5,700/min
