d716044db55fb3d89bdb1652fdc13f9ccadf392c
Carter Carlson <carterbud1@gmail.com>
2018-08-27 15:28:07 -0500

    HODL simulations now utilize numpy .dot function to improve efficiency

--- Simulate HODL.py
+++ Simulate HODL.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import random
-from openpyxl import load_workbook
 import os
 
 path = os.getcwd()
@@ -17,12 +16,6 @@ for num_to_select in range(2,11,2):
         random_list = random.sample(coin_list, num_to_select)
         coin_amts = [amt_each / df[i][0] for i in random_list]
         coins_chosen = '-'.join(random_list)
-
-        totals = []
-        temp  = np.array(df[random_list])
-
-        totals.append(sum(temp[num_to_select] * coin_amts))
-
-        simulations[coins_chosen] = totals
+        simulations[coins_chosen] = df[random_list].dot(coin_amts)
 
     simulations.to_csv(path + '/backtests/' + str(num_to_select) + '/' + str(num_to_select) + '_HODL.csv')
