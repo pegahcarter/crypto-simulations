d5644070b21e5acadac4e464ea67eaea7335da21
Carter Carlson <carterbud1@gmail.com>
2018-08-28 16:06:47 -0500

    Update HODL simulations to BTC/Change timeframe and # of coins used

--- Simulate HODL.py
+++ Simulate HODL.py
@@ -4,18 +4,28 @@ import random
 import os
 
 path = os.getcwd()
-file = path + '/historical_data.csv'
+file = path + '/historical prices.csv'
 df = pd.read_csv(file)
 coin_list = df.columns.tolist()[1:]
-start_amt = 5000
 
+# Start with $5000 of Bitcoin at day 0 price
+start_amt = 5000 / df['BTC'][0]
+
+# Simulate 2, 4, 6, 8, and 10 coins
 for num_to_select in range(2,11,2):
     amt_each = start_amt / num_to_select
-    simulations = pd.DataFrame()
-    for x in range(1000):
-        random_list = random.sample(coin_list, num_to_select)
-        coin_amts = [amt_each / df[i][0] for i in random_list]
-        coins_chosen = '-'.join(random_list)
-        simulations[coins_chosen] = df[random_list].dot(coin_amts)
+    simulations = pd.DataFrame({'date':df['date'].tolist()})
 
+    for x in range(1000):
+
+		# Randomly select coins to simulate
+        random_list = random.sample(coin_list, num_to_select)
+
+		# Get coin amount in Bitcoin denomination
+        coin_amts = [amt_each / df[i][0] for i in random_list]
+
+		# Record simulation
+        simulations['-'.join(random_list)] = df[random_list].dot(coin_amts)
+
+    simulations.set_index(['date'], inplace=True)
     simulations.to_csv(path + '/backtests/' + str(num_to_select) + '/' + str(num_to_select) + '_HODL.csv')
