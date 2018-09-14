7496a9527dde352c8379a06059f698e56259b082
Carter Carlson <carterbud1@gmail.com>
2018-09-14 15:02:16 -0500

    Get historical price data for larger coin set and date range

--- backtests/pull historical prices.py
+++ backtests/pull historical prices.py
@@ -4,9 +4,10 @@ import ccxt
 import numpy as np
 import pandas as pd
 
-# Returns dates and prices for the ticker: excludes high, low, volume, etc.
-def ticker_array(ticker):
-	return np.array(exchange.fetch_ohlcv(ticker, '1d'))[:,:2]
+# pull epoch dates used for market caps
+market_cap = pd.read_csv(os.getcwd() + '/backtests/historical market cap.csv')
+dates_epoch = market_cap['date']
+date_range = [time.strftime('%m/%d/%Y', time.localtime(day)) for day in dates_epoch]
 
 # Use coins listed on Bittrex
 primary_exchange = ccxt.bittrex({'options': {'adjustForTimeDifference': True}})
@@ -14,67 +15,42 @@ market = primary_exchange.load_markets()
 tickers = list(market.keys())
 
 coins = set()
-[[coins.add(coin) for coin in ticker.split('/')] for ticker in tickers]
+[[coins.add(coin) for coin in ticker.split('/') if coin != 'BTC'] for ticker in tickers]
 coins = list(coins)
 
 # Since we can't pull BTC/BTC, use BTC/USDT ticker.  Otherwise, use coin/BTC as ticker
-tickers = ['BTC/USDT'
-		   if coin == 'BTC'
-		   else coin + '/BTC'
-		   for coin in coins]
+tickers = [coin + '/BTC' for coin in coins]
+coins.insert(0, 'BTC')
+tickers.insert(0, 'BTC/USDT')
 
-# pull epoch dates used for market caps
-market_cap = pd.read_csv(os.getcwd() + '/backtests/historical market cap.csv')
-dates = market_cap['date'] * 1000.0
-
-data = np.array(exchange.fetch_ohlcv('BTC/USDT'))
-
-test = []
-for day, price in data[:, :2]:
-	if min(dates) <= day <= max(dates):
-		test.append([day, price])
-
-
-len(test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Use DOGE/BTC as basis to pull dates, because DOGE
-dates = []
-[dates.append(day) for day, price in ticker_array('DOGE/BTC') if 1501373000000 < day < 1532910000000] # July 30, 2017 to July 30, 2018
-df['date'] = dates
+df = [list(dates_epoch)]
+coins_to_simulate = []
 
 for coin, ticker in zip(coins, tickers):
 	# Pull information if ticker exists
 	try:
-		data = ticker_array(ticker)
+		data = np.array(primary_exchange.fetch_ohlcv(ticker, '1d'))[:, :2]
 	except:
 		continue
 
-	prices = []
-	[prices.append(price) for day, price in data if day in dates]
+	coin_prices = [price \
+				   for day, price in data \
+				   if time.strftime('%m/%d/%Y', time.localtime(day/1000)) in date_range]
 
 	# Only add coin if it has a full year of price data
-	if len(prices) == len(dates):
-		df[coin] = prices
+	if len(coin_prices) == len(dates):
+		df.append(coin_prices)
+		coins_to_simulate.append(coin)
 
-for col in df.columns.values[2:]:
-	df[col] *= df['BTC']
+# Add date to the front of coins to create column names
+coins_to_simulate.insert(0, 'date')
 
-df.set_index(['date'], drop=True, inplace=True)
-df.to_csv('historical prices.csv')
+df_np = np.array(df)
+
+# Since all coins are in BTC denomination, multiply by BTC price to get $ price
+df_np[2:] *= df[1]
+
+new_df = pd.DataFrame(df_np.transpose(), columns=coins_to_simulate)
+
+new_df.set_index('date', drop=True, inplace=True)
+new_df.to_csv(os.getcwd() + '/backtests/historical prices.csv')
