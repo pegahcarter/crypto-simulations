87d0abb3e5ad0c5d07ccf3aef2f237d06318d8ae
Carter Carlson <carterbud1@gmail.com>
2018-09-17 15:11:28 -0500

    Simulate HODL portfolios with updated date range

--- backtests/Simulate HODL.py
+++ backtests/Simulate HODL.py
@@ -8,33 +8,43 @@ import inspect
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename)) + '/backtests/'
 historical_prices = pd.read_csv(path + 'historical prices.csv')
+coins = historical_prices.columns.tolist()[1:]
 
-coin_list = historical_prices.columns.tolist()[1:]
-coin_indexes = len(coin_list)-1
+# get date ranges used for simulations
+historical_cap = pd.read_csv(path + 'historical market cap.csv')
+historical_cap = np.array(historical_cap)
 
-# Convert historical_prices to numpy array to improve performance
-data = np.array(historical_prices[coin_list])
+start_dates = historical_cap[:len(historical_cap) - 365]
+end_dates = historical_cap[365:]
+
+# Subtract the ending market caps from each other, located in the 4th column
+cap_diffs = list(end_dates[:, 3] - start_dates[:, 3])
+
+# Make sure there's an odd number of dates, so the median value can be indexed
+if len(cap_diffs) % 2 == 0:
+	cap_diffs.pop(len(cap_diffs)-1)
+
+# Start date for simulations
+start_date = cap_diffs.index(np.median(cap_diffs))
+
+# Limit dataframe dates to the date range
+data = np.array(historical_prices[coins])
+data = data[start_date:start_date + 366]
 
 # Start with $5000 of Bitcoin at day 0 price
 start_amt = 5000
 
 # Simulate 2, 4, 6, 8, and 10 coins
-for num_to_select in range(2,11,2):
-    amt_each = start_amt / num_to_select
-    simulations = pd.DataFrame({'date':historical_prices['date'].tolist()})
+for num_coins in range(2,11,2):
+	amt_each = start_amt / num_coins
+	simulations = pd.DataFrame()
 
-	# Run 1000 simulations
-    for x in range(1000):
+	for sim_num in range(1000):
+		random_list = random.sample(range(len(coins)-1), num_coins)
+		coin_amts = amt_each / data[0, random_list]
 
-		# Randomly select coins to simulate
-        random_list = random.sample(range(0, coin_indexes), num_to_select)
-        col_name = '-'.join([coin_list[i] for i in random_list])
+		col = '-'.join([coins[i] for i in random_list])
 
-		# Get coin amount
-        coin_amts = amt_each / data[0][random_list]
+		simulations[col] = data[:, random_list].dot(coin_amts)
 
-		# Record simulation
-        simulations[col_name] = data[:, random_list].dot(coin_amts)
-
-    simulations.set_index(['date'], inplace=True)
-    simulations.to_csv(path + str(num_to_select) + '/' + str(num_to_select) + '_HODL.csv')
+	simulations.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_HODL.csv')
