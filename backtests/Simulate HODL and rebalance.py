b87482ec716e6249ae2208aa14415d9ae49fefbb
Carter Carlson <carterbud1@gmail.com>
2018-10-24 16:08:45 -0500

    Refactor for readability

--- backtests/Simulate HODL and rebalance.py
+++ backtests/Simulate HODL and rebalance.py
@@ -8,18 +8,18 @@ import random
 
 
 def simulate_HODL():
-	simulations = pd.DataFrame(index=sim_dates)
+	sims = pd.DataFrame(index=sim_dates)
 
 	for sim_num in range(1000):
 		random_list = random.sample(range(len(coins)-1), num_coins)
-		coin_amts = amt_each / historical_prices[0, random_list]
+		coin_amts = amt_each / hist_prices[0, random_list]
 
 		col = '-'.join([coins[i] for i in random_list])
 
-		simulations[col] = historical_prices[:, random_list].dot(coin_amts)
+		sims[col] = hist_prices[:, random_list].dot(coin_amts)
 
-	simulations.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_HODL.csv')
-	return simulations
+	sims.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_HODL.csv')
+	return sims
 
 
 def simulate_rebalance(df):
@@ -31,41 +31,41 @@ def simulate_rebalance(df):
 
 	# Exclude date column
 	cols = df.columns.tolist()
-	hodl_simulations = np.array(df)
+	hodl_sims = np.array(df)
 
 	# Create arrays to be transformed to CSV's
-	simulation_summary = [[] for x in range(len(cols))]
-	rebalance_simulations = np.empty(shape=(len(cols), len(historical_prices)))
-	num_simulation = 0
+	sim_summary = [[] for x in range(len(cols))]
+	rebalance_sims = np.empty(shape=(len(cols), len(hist_prices)))
+
 
 	# Use the same coin combinations as the HODL simulation
 	coin_lists = [col.split('-') for col in cols]
 
-	# For each simulation, convert the symbol into the corresponding column # in historical_prices
-	coin_lists_indexes = [[coins.index(coin) for coin in coin_list] for coin_list in coin_lists]
+	# For each simulation, convert the symbol into the corresponding column # in hist_prices
+	coin_lists_indices = [[coins.index(coin) for coin in coin_list] for coin_list in coin_lists]
 
-	for col, coin_list, coin_list_index in zip(cols, coin_lists, coin_lists_indexes):
+	for num, (col, coin_list, coin_list_index) in enumerate(zip(cols, coin_lists, coin_lists_indices)):
 
 		fees, trade_count, trades_eliminated, taxes_owed = 0, 0, 0, 0
 		daily_totals = [start_amt]
 
-		# Reduce historical_prices array to only the coins used in the simulation
-		small_historical_prices = historical_prices[:, coin_list_index]
+		# Reduce hist_prices array to only the coins used in the simulation
+		hist_prices_small = hist_prices[:, coin_list_index]
 
 		# Calculate starting coin amounts
-		coin_amts = amt_each / small_historical_prices[0]
+		coin_amts = amt_each / hist_prices_small[0]
 
-		purchase_prices = small_historical_prices[0].tolist()
+		purchase_prices = hist_prices_small[0].tolist()
 
 		# Simulate each day
-		for num_day in range(1,len(historical_prices)):
+		for num_day in range(1,len(hist_prices)):
 			while True:
-				dollar_values = small_historical_prices[num_day] * coin_amts
-				total_dollar_value = sum(dollar_values)
-				l_index, h_index = dollar_values.argmin(), dollar_values.argmax()
+				d_vals = hist_prices_small[num_day] * coin_amts
+				d_vals_total = sum(d_vals)
+				l_index, h_index = d_vals.argmin(), d_vals.argmax()
 
 				# See how far the lightest and heaviest coin weight deviates from average weight
-				weight_to_move = min([avg_weight - dollar_values[l_index]/total_dollar_value, dollar_values[h_index]/total_dollar_value - avg_weight])
+				weight_to_move = min([avg_weight - d_vals[l_index]/d_vals_total, d_vals[h_index]/d_vals_total - avg_weight])
 
 				if weighted_thresh > weight_to_move:
 					break
@@ -82,39 +82,38 @@ def simulate_rebalance(df):
 					rate = 0.0025
 					trades_eliminated += 1
 
-				dollar_amt = weight_to_move * total_dollar_value
-				fees += (dollar_amt * rate)
+				d_amt = weight_to_move * d_vals_total
+				fees += (d_amt * rate)
 
 				# Get coin quantities to buy/sell based on current market price
-				l_quantity = dollar_amt / small_historical_prices[num_day, l_index]
-				h_quantity = dollar_amt / small_historical_prices[num_day, h_index] * (1 + rate)
+				l_quantity = d_amt / hist_prices_small[num_day, l_index]
+				h_quantity = d_amt / hist_prices_small[num_day, h_index] * (1 + rate)
 
-				price_difference = small_historical_prices[num_day, h_index] - purchase_prices[h_index]
+				price_diff = hist_prices_small[num_day, h_index] - purchase_prices[h_index]
 
-				taxes_owed += (price_difference * h_quantity * 0.25)
+				taxes_owed += (price_diff * h_quantity * 0.25)
 
 				# adjust avg purchase price for bought coin
-				purchase_prices[l_index] = (purchase_prices[l_index] * coin_amts[l_index] + small_historical_prices[num_day, l_index] * l_quantity)/(coin_amts[l_index] + l_quantity)
+				purchase_prices[l_index] = (purchase_prices[l_index] * coin_amts[l_index] + hist_prices_small[num_day, l_index] * l_quantity)/(coin_amts[l_index] + l_quantity)
 
 				# Adjust coin quantities
 				coin_amts[l_index] += l_quantity
 				coin_amts[h_index] -= h_quantity
 
 			# document total portfolio value on that day
-			daily_totals.append(np.dot(small_historical_prices[num_day], coin_amts))
+			daily_totals.append(np.dot(hist_prices_small[num_day], coin_amts))
 
-		# Document important features of the simulations
-		end_price_HODL = hodl_simulations[len(hodl_simulations)-1, num_simulation]
+		# Document important features of the sims
+		end_price_HODL = hodl_sims[len(hodl_sims)-1, num]
 		end_price_rebalanced = daily_totals[len(daily_totals)-1]
-		simulation_summary[num_simulation] = [col, fees, taxes_owed, trade_count, trades_eliminated, end_price_HODL, end_price_rebalanced]
-		rebalance_simulations[num_simulation] = daily_totals
-		num_simulation += 1
+		sim_summary[num] = [col, fees, taxes_owed, trade_count, trades_eliminated, end_price_HODL, end_price_rebalanced]
+		rebalance_sims[num] = daily_totals
 
-	rebalance_simulations = pd.DataFrame(np.transpose(rebalance_simulations), columns=cols, index=sim_dates)
-	rebalance_simulations.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_rebalanced.csv')
+	rebalance_sims = pd.DataFrame(np.transpose(rebalance_sims), columns=cols, index=sim_dates)
+	rebalance_sims.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_rebalanced.csv')
 
-	simulation_summary = pd.DataFrame(simulation_summary,columns=['portfolio','total_fees','taxes_owed','num_trades','num_trades_saved','end_price_HODL','end_price_rebalanced'])
-	simulation_summary.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_summary.csv', index=False)
+	sim_summary = pd.DataFrame(sim_summary,columns=['portfolio','total_fees','taxes_owed','num_trades','num_trades_saved','end_price_HODL','end_price_rebalanced'])
+	sim_summary.to_csv(path + str(num_coins) + '/' + str(num_coins) + '_summary.csv', index=False)
 
 
 if __name__ == '__main__':
@@ -122,20 +121,20 @@ if __name__ == '__main__':
 	# Retrieve location of historical prices file
 	filename = inspect.getframeinfo(inspect.currentframe()).filename
 	path = os.path.dirname(os.path.abspath(filename)) + '/backtests/'
-	historical_prices = pd.read_csv(path + 'historical prices.csv')
+	hist_prices = pd.read_csv(path + 'historical prices.csv')
 
-	sim_dates = list(historical_prices['date'])
-	coins = historical_prices.columns.tolist()[1:]
+	sim_dates = list(hist_prices['date'])
+	coins = hist_prices.columns.tolist()[1:]
 
 	# Exclude date column from historical prices
-	historical_prices = np.array(historical_prices[coins])
+	hist_prices = np.array(hist_prices[coins])
 
-	# get date ranges used for simulations
-	historical_cap = pd.read_csv(path + 'historical market cap.csv')
-	historical_cap = np.array(historical_cap)
+	# get date ranges used for sims
+	hist_cap = pd.read_csv(path + 'historical market cap.csv')
+	hist_cap = np.array(hist_cap)
 
-	start_dates = historical_cap[:len(historical_cap) - 365]
-	end_dates = historical_cap[365:]
+	start_dates = hist_cap[:len(hist_cap) - 365]
+	end_dates = hist_cap[365:]
 
 	# Subtract the ending market caps from each other, located in the 4th column
 	cap_diffs = list(end_dates[:, 3] - start_dates[:, 3])
@@ -144,11 +143,11 @@ if __name__ == '__main__':
 	if len(cap_diffs) % 2 == 0:
 		cap_diffs.pop(len(cap_diffs)-1)
 
-	# Start date for simulations
+	# Start date for sims
 	start_date = cap_diffs.index(np.median(cap_diffs))
 
 	# Limit dataframe dates to the date range
-	historical_prices = historical_prices[start_date:start_date + 365]
+	hist_prices = hist_prices[start_date:start_date + 365]
 	sim_dates = sim_dates[start_date:start_date + 365]
 
 	# Retrieve all current tickers on exchange
